5bc96a0f71aa5120c2556d0cf7ec8e837afa56a6
steer-xyz <steer.xyz@gmail.com>
2025-06-16 10:36:03 -0400

    cli - fix main.py

--- src/contribu/cli/main.py
+++ src/contribu/cli/main.py
@@ -1,12 +1,11 @@
 import typer
-from contribu.cli.commands import fetch_repos, init, suggest_prs, me
+from contribu.cli.commands import fetch_repos, init, me
 
 app = typer.Typer()
 
 app.command()(init.init)
 app.command()(me.me)
-app.command()(fetch_repos.fetch_repos)
-# app.command()(suggest_prs.run)
+app.command()(fetch_repos.find_repos)
 
 if __name__ == "__main__":
     app()
